5c9ab7c38910f15320b722e4395cc20f9a2283e3
Andreas Rauschert <andreas.rb.rauschert@bmw.de>
2021-08-02 15:44:03 +0200

    Temporary comment openPASS execution in script for 1.1 scenarios
    Signed-off-by: Andreas Rauschert <andreas.rb.rauschert@bmw.de>

--- Scenarios/run_Scenario.bat
+++ Scenarios/run_Scenario.bat
@@ -11,13 +11,15 @@ SET sel=0
 ECHO Please select simulation tool:
 ECHO [0]: Abort program
 ECHO [1]: esmini
-ECHO [2]: openPASS
+:: TODO: Add again once openPASS can execute OpenSCENARIO 1.1
+::ECHO [2]: openPASS
 SET /p sel="Enter ID: "
 
 IF %sel% LSS 1 GOTO finish
-IF %sel% GTR 2 GOTO finish
+::IF %sel% GTR 2 GOTO finish
+IF %sel% GTR 1 GOTO finish
 IF %sel% EQU 1 GOTO esmini
-IF %sel% EQU 2 GOTO OpenPASS
+::IF %sel% EQU 2 GOTO OpenPASS
 
 :esmini
 
